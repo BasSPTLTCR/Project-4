6c69ba20735215ffc3c99a571ff783fb0fa5bc20
FlorisBreemer <9018644@student.zadkine.nl>
2023-05-24 16:34:19 +0200

    landen toevoegen klaar

--- index.php
+++ index.php
@@ -6,16 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BEFS</title>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./style.css">  
 </head>
-<nav>
-        <ul>
-            <li><a href="./leverancier_toevoegen.php">Leverancier toevoegen</a></li>
-            <li><a href="./category_toevoegen.php">Category toevoegen</a></li>
-            <li><a href="./landen_toevoegen.php">Landen toevoegen</a></li>
-            <li><a href="./beheerder_toevoegen.php">Beheerder toevoegen</a></li>
-        </ul>
-    </nav> 
 <body>
     <?php
     include_once "./includes/nav.html";
--- landen_toevoegen.php
+++ landen_toevoegen.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,60 +8,88 @@
     <title>C01-02 Landen Toevoegen</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-<?php
-require 'db-connection.php';
+    <?php
+    require 'db-connection.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $code = $_POST["code"];
+    if (strtoupper($_SERVER["REQUEST_METHOD"]) == "POST") {
+        if (isset($_POST['agreed'])) { //User heeft dus nog geen akkoord gegeven...
+            ?>
 
- // Prepare the query
- $query = "SELECT COUNT(name) AS count_name FROM country WHERE name = :name";
+            U staat op het punt om
+            <?php echo $_POST['name']; ?> met code
+            <?php echo $_POST['code']; ?> aan te maken.
 
- // Prepare the statement
- $statement = $conn->prepare($query);
+            Weet u dat zeker?
+            <form method="post">
+                <input type="hidden" name="name" value="<?php echo $_POST['name']; ?>">
 
- // Bind the email parameter
- $statement->bindParam(':name', $name);
+                <input type="hidden" name="code" value="<?php echo $_POST['name']; ?>">
 
- // Execute the statement
- $statement->execute();
+                <input type="hidden" nme="agreed" value="1" />
+                <input type="submit" value="JA">
+                <button value="NEE" onclick="javascript:history.go(-1);">NEE</button>
+            </form>
 
- // Fetch the result
- $result = $statement->fetch(PDO::FETCH_ASSOC);
+            <?php
+        } else {
+            $name = $_POST["name"];
+            $code = $_POST["code"];
 
- // Access the value of 'count_email'
- $countName = $result['count_name'];
+            // Prepare the query
+            $query = "SELECT name FROM country WHERE name = :name";
+            $query2 = "SELECT code FROM country WHERE code = :code";
 
- if ($countName == 0) {
-    $sql = "INSERT INTO country (name, code) VALUES (:name, :code)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':name', $name);
-    $stmt->bindParam(':code', $code);
+            // Prepare the statement
+            $statement = $conn->prepare($query);
+            $statement2 = $conn->prepare($query2);
 
-    try {
-        $stmt->execute();
-        echo "Land succesvol toegevoegd.";
-    } catch (PDOException $e) {
-        echo "Fout bij het toevoegen van het land: " . $e->getMessage();
+            // Bind the name and code parameter
+            $statement->bindParam(':name', $name);
+            $statement2->bindParam(':code', $code);
+
+            // Execute the statement
+            $statement->execute();
+            $statement2->execute();
+
+            if ($statement->RowCount() === 0) {
+                if ($statement2->RowCount() === 0) {
+                    $sql = "INSERT INTO country (name, code) VALUES (:name, :code)";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->bindParam(':name', $name);
+                    $stmt->bindParam(':code', $code);
+
+                    try {
+                        $stmt->execute();
+                        echo "Land succesvol toegevoegd.";
+                    } catch (PDOException $e) {
+                        echo "Fout bij het toevoegen van het land: " . $e->getMessage();
+                    }
+                } else {
+                    echo "Er bestaat al een land met deze code.";
+                }
+            } else {
+                echo "Er bestaat al een land met deze naam";
+            }
+        }
+    } else {
+        ?>
+        <form method="post">
+            <label>Naam:</label>
+            <input type="text" name="name" required><br>
+
+            <label>Code:</label>
+            <input type="text" name="code" required><br>
+
+            <input type="submit" value="Landen toevoegen">
+        </form>
+        <?php
+
     }
-}
-    else  {
-        echo "Er bestaat al een land met deze naam";
-}}
-?>
-
-<form method="post">
-    <label>Naam:</label>
-    <input type="text" name="name" required><br>
-
-    <label>Code:</label>
-    <input type="text" name="code" required><br>
-
-    <input type="submit" value="Landen toevoegen">
-</form>
+    ?>
 
 
 </body>
+
 </html>
